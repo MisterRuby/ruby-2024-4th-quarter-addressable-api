d013ef1d4af3de03a1223a8789605214b02226ce
Ruby <93859705+MisterRuby@users.noreply.github.com>
2024-09-29 22:16:58 +0900

    init : 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ruby-2024-4th-quarter-addressable-api"
---
+++ src/test/kotlin/ruby/ruby20244thquarteraddressableapi/Ruby20244thQuarterAddressableApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ruby.ruby20244thquarteraddressableapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ruby20244thQuarterAddressableApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
